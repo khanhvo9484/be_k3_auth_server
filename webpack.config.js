114c2f77cffd22851807b050e1f328b936c695a8
20120507 <103519164+khanhvo9484@users.noreply.github.com>
2023-12-12 13:39:03 +0700

    change registry11

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,25 @@
-const webpack = require('webpack')
-const path = require('path')
 const nodeExternals = require('webpack-node-externals')
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin')
 
-const ProgressBarPlugin = require('progress-bar-webpack-plugin')
-const BundleAnalyzerPlugin =
-	require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-
-module.exports = {
-	entry: ['webpack/hot/poll?100', './src/main.ts'],
-	watch: true,
-	target: 'node',
-	externals: [
-		nodeExternals({
-			whitelist: ['webpack/hot/poll?100']
-		})
-	],
-	module: {
-		rules: [
-			{
-				test: /.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			}
+module.exports = function (options, webpack) {
+	return {
+		...options,
+		entry: ['webpack/hot/poll?100', options.entry],
+		externals: [
+			nodeExternals({
+				allowlist: ['webpack/hot/poll?100']
+			})
+		],
+		plugins: [
+			...options.plugins,
+			new webpack.HotModuleReplacementPlugin(),
+			new webpack.WatchIgnorePlugin({
+				paths: [/\.js$/, /\.d\.ts$/]
+			}),
+			new RunScriptWebpackPlugin({
+				name: options.output.filename,
+				autoRestart: false
+			})
 		]
-	},
-	mode: 'development',
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js']
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/]),
-		new ProgressBarPlugin({
-			complete: '▰',
-			incomplete: '▱',
-			clear: false
-		}),
-		new BundleAnalyzerPlugin({
-			analyzerMode: 'static',
-			analyzerHost: '127.0.0.1',
-			analyzerPort: '8888',
-			reportFilename: process.env.NODE_ENV === 'development' && 'report.html',
-			openAnalyzer: false,
-			generateStatsFile: false,
-			statsFilename: 'stats.json'
-		}),
-		new webpack.BannerPlugin({
-			banner: 'require("source-map-support").install();',
-			raw: true,
-			entryOnly: false
-		})
-	],
-	optimization: {
-		removeAvailableModules: false,
-		removeEmptyChunks: false,
-		splitChunks: false
-	},
-	output: {
-		pathinfo: false
-		// path: path.join(__dirname, 'dist'),
-		// filename: 'server.js'
 	}
 }
